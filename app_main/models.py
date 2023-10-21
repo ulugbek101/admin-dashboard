55c6efe2a4b2de87988ee27b0cdda6d7f61a4b2d
Ulug'bek Umaraliyev <thedevu101@gmail.com>
2023-10-22 02:39:12 +0500

    Update models.py, staticfiles, requirements.txt
    - Update models.py
    - Update requirements.txt
    - Collect staticfiles

--- app_main/models.py
+++ app_main/models.py
@@ -17,8 +17,8 @@ class Subject(models.Model):
 
 
 class Group(models.Model):
-    subject = models.ForeignKey(to=Subject, on_delete=models.SET_NULL, null=True, verbose_name='Fan nomi')
-    teacher = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=True, verbose_name='O\'qituvchisi')
+    subject = models.ForeignKey(to=Subject, on_delete=models.PROTECT, verbose_name='Fan nomi')
+    teacher = models.ForeignKey(to=User, on_delete=models.PROTECT, verbose_name='O\'qituvchisi')
     name = models.CharField(max_length=200, verbose_name='Guruh nomi', unique=True)
     price = models.IntegerField(default=0, verbose_name='Guruh to\'lovi')
     created = models.DateTimeField(auto_now_add=True, null=True)
@@ -32,7 +32,7 @@ class Group(models.Model):
 class Pupil(models.Model):
     first_name = models.CharField(max_length=200, verbose_name='Ism')
     last_name = models.CharField(max_length=200, verbose_name='Familiya')
-    group = models.ForeignKey(to=Group, on_delete=models.SET_NULL, null=True)
+    group = models.ForeignKey(to=Group, on_delete=models.PROTECT)
     created = models.DateTimeField(auto_now_add=True, null=True)
     updated = models.DateTimeField(auto_now=True, null=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
@@ -64,6 +64,7 @@ class Pupil(models.Model):
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
 
+
 # class Payment(models.Model):
 #     owner = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=True)
 #     month = models.CharField(max_length=200, null=True)
@@ -74,10 +75,10 @@ class Pupil(models.Model):
 #     created = models.DateTimeField(auto_now_add=True, null=True, verbose_name='To\'lov vaqti')
 #     updated = models.DateTimeField(auto_now=True, null=True, verbose_name='O\'zgartirilgan vaqt')
 #     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
-
-# @property
-# def is_changed(self) -> bool:
-#     return self.created != self.updated
 #
-# def __str__(self) -> str:
-#     return f'{self.pupil} - {self.amount}'
+#     @property
+#     def is_changed(self) -> bool:
+#         return self.created != self.updated
+#
+#     def __str__(self) -> str:
+#         return f'{self.pupil} - {self.amount}'
