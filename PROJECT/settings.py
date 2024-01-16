ff9b5a900e93401aa0123c8d629ff20a21a9a693
Ulugbek Umaraliyev <thedevu101@gmail.com>
2024-01-16 15:58:18 +0000

    Update settings.py, Change staticfiles source

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -122,11 +122,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = 'thedevu101-admin-media/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static'
+# ]
 
-# STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
