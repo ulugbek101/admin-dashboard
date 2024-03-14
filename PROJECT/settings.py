f39f96bc726a2095d0be57ca4c0e1d3fb79f2d9e
Ulugbek Umaraliyev <thedevu101@gmail.com>
2024-03-14 02:52:34 +0000

    Update ALLOWED_HOSTS and MEDIA_URL

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -10,12 +10,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-8e)*boeb4v6_$h7v79qv)x+s9j3$ujm$js$92i-e2o388%6ld)'
 CORS_ORIGIN_ALLOW_ALL = True
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['www.thedevu101.online', 'thedevu101.online', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['www.al-xorazmiy.online', 'al-xorazmiy.online', 'localhost', '127.0.0.1']
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://thedevu101.online',
+    'https://al-xorazmiy.online',
 ]
 
 INSTALLED_APPS = [
@@ -129,7 +129,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-MEDIA_URL = 'thedevu101-admin/'
+MEDIA_URL = 'al-xorazmiy-media/'
 
 # STATICFILES_DIRS = [
 #     BASE_DIR / 'static'
