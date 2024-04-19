0d4bd0de7c7d7f563ac15c00d82ebb8de487904f
ulugbek101 <thedevu101@gmail.com>
2024-04-20 04:03:14 +0500

    Update settings.py

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = 'django-insecure-8e)*boeb4v6_$h7v79qv)x+s9j3$ujm$js$92i-e2o388%6ld)
 CORS_ORIGIN_ALLOW_ALL = True
 DEBUG = False
 
-ALLOWED_HOSTS = ['shams-school.online', 'shams-school.uz', 'www.shams-school.online', 'www.shams-school.uz', 'localhost', '127.0.0.1', '137.184.90.161']
+ALLOWED_HOSTS = ['thedevu101.online', 'www.thedevu101.online', 'localhost', '127.0.0.1', '137.184.90.161']
 
 CSRF_TRUSTED_ORIGINS = [
     'https://shams-school.online',
