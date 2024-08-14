2c4a4b5a40ec57c3a54280a535699dcd719762c2
ulugbek101 <thedevu101@gmail.com>
2024-08-15 03:58:38 +0500

    Update settings.py

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -12,9 +12,10 @@ SECRET_KEY = 'django-insecure-8e)*boeb4v6_$h7v79qv)x+s9j3$ujm$js$92i-e2o388%6ld)
 CORS_ORIGIN_ALLOW_ALL = True
 DEBUG = False
 
-ALLOWED_HOSTS = ['shams-school.uz', 'www.shams-school.uz', 'localhost', '127.0.0.1', '137.184.90.161']
+ALLOWED_HOSTS = ['shams-school.online', 'shams-school.uz', 'www.shams-school.online', 'www.shams-school.uz', 'localhost', '127.0.0.1', '137.184.90.161']
 
 CSRF_TRUSTED_ORIGINS = [
+    'https://shams-school.online',
     'https://shams-school.uz',
 ]
 
@@ -129,7 +130,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-MEDIA_URL = 'al-xorazmiy-media/'
+MEDIA_URL = 'shams-media/'
 
 # STATICFILES_DIRS = [
 #     BASE_DIR / 'static'
